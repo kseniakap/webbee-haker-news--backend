7d8bb988f48c3f604d2cdffe16e9cb44035cd5e7
ksenia_kap_1 <kseniakap096@gmail.com>
2024-08-07 22:06:20 +0300

    fix: Правка замечаний

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4000;
+
 async function server() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
 server();
